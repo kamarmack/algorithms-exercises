390e0cfa273b5858776c5c67c4f8a434c7d5d9ec
kamarmack <kamarmack112@gmail.com>
2021-12-30 03:48:03 -0600

    completes insertion sort

--- src/Sort.js
+++ src/Sort.js
@@ -4,13 +4,8 @@ import { App, snapshot, done, clear } from "./sort-visualizer";
 
 import "./sort.css";
 
-function sort(array) {
-  // do cool stuff here
-
-  // call snapshot any time you do anything to the array
-  // it's okay if you call it with duplicate value array,
-  // it will deduplicate for you
-  snapshot(array);
+function sort(nums) {
+  return nums;
 }
 
 export default function SortComponent() {
