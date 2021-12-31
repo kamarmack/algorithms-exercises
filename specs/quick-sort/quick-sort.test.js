e901958b45905e6d4d27ca2b90ccdbb2f2851dc6
kamarmack <kamarmack112@gmail.com>
2021-12-30 18:58:58 -0600

    completes quicksort

--- specs/quick-sort/quick-sort.test.js
+++ specs/quick-sort/quick-sort.test.js
@@ -3,22 +3,40 @@
   Quick Sort!
   
   Name your function quickSort.
-  
-  Quick sort should grab a pivot from the end and then separate the list (not including the pivot)
-  into two lists, smaller than the pivot and larger than the pivot. Call quickSort on both of those
-  lists independently. Once those two lists come back sorted, concatenate the "left" (or smaller numbers)
-  list, the pivot, and the "right" (or larger numbers) list and return that. The base case is when quickSort
-  is called on a list with length less-than-or-equal-to 1. In the base case, just return the array given.
 
 */
 
+/**
+ * Quick sort should grab a pivot from the end and then separate the list
+ * (not including the pivot) into two lists, smaller than the pivot and
+ * larger than the pivot. Call quickSort on both of those lists
+ * independently. Once those two lists come back sorted, concatenate the
+ * "left" (or smaller numbers) list, the pivot, and the "right" (or larger
+ * numbers) list and return that. The base case is when quickSort is
+ * called on a list with length less-than-or-equal-to 1. In the base
+ * case, just return the array given.
+ * @param {array} nums 
+ */
 function quickSort(nums) {
-  // code goes here
+  if (nums.length < 2) {
+    return nums;
+  }
+  const p = nums.pop();
+  const left = [];
+  const right = [];
+  nums.forEach(val => {
+    if (val < p) {
+      left.push(val);
+    } else {
+      right.push(val);
+    }
+  });
+  return [...quickSort(left), p, ...quickSort(right)];
 }
 
 // unit tests
 // do not modify the below code
-test.skip("quickSort", function () {
+test("quickSort", function () {
   const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
   const answer = quickSort(input);
 
